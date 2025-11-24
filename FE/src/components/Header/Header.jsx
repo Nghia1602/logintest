dc8973d227dd5305c86b95c502891be081a568e2
Nghia1602 <2002nghiantt@gmail.com>
2025-11-24 20:28:38 +0700

    24/11_2

--- FE/src/components/Header/Header.jsx
+++ FE/src/components/Header/Header.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const HeaderLayout = () => {
   return (
     <div className="w-[100%] h-full flex items-center ">
-      <div className="flex flex-2 font-bold text-[28px] uppercase text-[#4589b6ff] pl-4  flex items-center">
-        <div className="border-r-[1px] border-[#4589b6ff] h-[28px] text-center ">
+      <div className="flex flex-2 font-bold text-[25px] uppercase text-[#4589b6ff] pl-4  flex items-center">
+        <div className="border-r-[3px] border-[#4589b6ff] h-[28px] flex items-center pr-2">
           Phần mềm quản lý
+          <span></span>
         </div>
       </div>
       <div className="flex flex-1"></div>
--- FE/src/components/Sider/Sider.jsx
+++ FE/src/components/Sider/Sider.jsx
@@ -1,7 +1,13 @@
-import React, {useState} from "react";
-import { Layout, Menu } from 'antd';
+import React, { useState } from "react";
+import { Layout, Menu } from "antd";
 import { Link } from "react-router-dom";
-import { House,Contact,BookOpen,SquareUser,MessageCircleMore,} from 'lucide-react';
+import {
+  House,
+  Contact,
+  BookOpen,
+  SquareUser,
+  MessageCircleMore,
+} from "lucide-react";
 const { Header, Content, Footer, Sider } = Layout;
 function getItem(label, key, icon, children) {
   return {
@@ -14,17 +20,40 @@ function getItem(label, key, icon, children) {
 const SiderLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
   const items = [
-  getItem(<Link to="/home">Trang chủ</Link>, '1',<House/>),
-  getItem(<Link to="/list-student">Danh sách lớp</Link>, '2',<Contact />),
-  getItem('Môn học', '3',<BookOpen />),
-  getItem('Giáo viên', '4',<SquareUser /> ),
-  getItem('Chat', '9', <MessageCircleMore />),
-];
-  return <Sider collapsible   width="250px" style={{background:"#ffffff" }} >
-        <div className=" flex items-center w-[250px] h-[75px] justify-center border-b-[1px] border-gray-200 shadow-md ">
-            <img src="logo_thaco_industries.png" alt="" style ={{height:"40px", display:"flex", }}/>
-        </div>
-        <Menu theme="light" defaultSelectedKeys={['1']} mode="inline" items={items} style={{width:"100%",borderInlineEnd:"0px"}}  />
-      </Sider>
+    getItem(<Link to="/home">Trang chủ</Link>, "1", <House />),
+    getItem(<Link to="/list-student">Danh sách lớp</Link>, "2", <Contact />),
+    getItem("Môn học", "3", <BookOpen />),
+    getItem("Giáo viên", "4", <SquareUser />),
+    getItem("Chat", "9", <MessageCircleMore />),
+  ];
+  return (
+    <Sider
+      collapsible
+      collapsed={collapsed}
+      onCollapse={(value) => setCollapsed(value)}
+      width="250px"
+      style={{ background: "#ffffff" }}
+    >
+      <div className=" flex items-center  h-[75px] justify-center border-b-[1px] border-gray-200 shadow-md ">
+        {collapsed ? <img
+          src="logo_industries.png"
+          alt=""
+          style={{ height: "50px", display: "flex" }}
+        />:<img
+          src="logo_thaco_industries.png"
+          alt=""
+          style={{ height: "40px", display: "flex" }}
+        />}
+        
+      </div>
+      <Menu
+        theme="light"
+        defaultSelectedKeys={["1"]}
+        mode="inline"
+        items={items}
+        style={{ width: "100%", borderInlineEnd: "0px" }}
+      />
+    </Sider>
+  );
 };
 export default SiderLayout;
